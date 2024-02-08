266b8f6e957c4c3fc0cb23a08034f697b148710a
Mwembe-Tsuma <49154717+Mwembe-Tsuma@users.noreply.github.com>
2024-02-08 11:47:14 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,1 +1,54 @@
+#!/usr/bin/python3
+"""N queens"""
+import sys
 
+
+def is_safe(board, row, col, n):
+    """Check if there is a queen in the same column"""
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
+
+
+def print_solution(board):
+    """Print solution"""
+    print([[i, board[i]] for i in range(len(board))])
+
+
+def solve_nqueens(board, row, n):
+    """Solve it"""
+    if row == n:
+        print_solution(board)
+        return
+
+    for col in range(n):
+        if is_safe(board, row, col, n):
+            board[row] = col
+            solve_nqueens(board, row + 1, n)
+
+
+def nqueens(n):
+    """Check input"""
+    if not n.isdigit():
+        print("N must be a number")
+        sys.exit(1)
+
+    n = int(n)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [-1] * n
+    solve_nqueens(board, 0, n)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    nqueens(sys.argv[1])
