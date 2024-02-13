db808e6b3767db5efd98e8b681de57af7bfe9308
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-02-13 13:20:18 +0000

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,44 +2,26 @@
 
 const request = require('request');
 
-function getCharacters(movieId) {
-  const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-  request(apiUrl, { json: true }, (err, res, body) => {
+const getCharacters = (arr, idx) => {
+  if (idx === arr.length) return;
+  request(arr[idx], (err, response, body) => {
     if (err) {
-      console.error('Error fetching data:', err);
-      return;
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      getCharacters(arr, idx + 1);
     }
-
-    if (res.statusCode !== 200) {
-      console.error(`Unexpected status code: ${res.statusCode}`);
-      return;
-    }
-
-    const characters = body.characters;
-
-    characters.forEach(characterUrl => {
-      request(characterUrl, { json: true }, (err, res, characterBody) => {
-        if (err) {
-          console.error('Error fetching character data:', err);
-          return;
-        }
-
-        if (res.statusCode !== 200) {
-          console.error(`Unexpected status code: ${res.statusCode}`);
-          return;
-        }
-
-        console.log(characterBody.name);
-      });
-    });
   });
-}
+};
 
-const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error('Please provide a movie ID as a command line argument.');
-} else {
-  getCharacters(movieId);
-}
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      getCharacters(chars, 0);
+    }
+  }
+);
